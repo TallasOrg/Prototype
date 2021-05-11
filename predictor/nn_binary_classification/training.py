cff7c97f9db567c0fc68355fa1aed80eeb3a4bc7
jahmanson <justin.ahmanson@tallas.io>
2021-05-11 16:58:11 -0500

    more data, binary classification results

---
+++ predictor/nn_binary_classification/training.py
@@ -0,0 +1,59 @@
+import time
+import torch
+import torch.nn as nn
+import numpy as np
+import matplotlib.pyplot as plt
+from torch.utils.tensorboard import SummaryWriter
+from utils import HueskenDataset, ToTensor
+from model import nnBinaryClassification
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+dataset = HueskenDataset(datafile='../../data/HueskenRNA_success.csv', transform=ToTensor())
+
+hidden_layers = [1, 2, 3]
+hidden_sizes = [22, 44, 88]
+for num_hidden_layers in hidden_layers:
+    for hidden_size in hidden_sizes:
+        input_size = 22
+        num_epochs = 5
+        batch_size = 1
+        learning_rate = 0.001
+        optimizer_type = 'adam'
+
+        LOG_NAME = f'{hidden_size}-nodes-{num_hidden_layers}-dense-{learning_rate}-lr-{optimizer_type}-optim-{int(time.time())}'
+        writer = SummaryWriter(f'logs/{LOG_NAME}')
+
+        loader = torch.utils.data.DataLoader(dataset=dataset, batch_size=batch_size, shuffle=True)
+        examples = iter(loader)
+        example_features, example_labels = examples.next()
+
+        model = nnBinaryClassification(input_size, hidden_size, num_hidden_layers)
+        criterion = nn.BCELoss()
+        if optimizer_type == 'adam':
+            optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+        else:
+            optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
+
+        writer.add_graph(model, example_features)
+
+        n_total_steps = len(loader)
+        running_loss = 0.0
+        for epoch in range(num_epochs):
+            for i, (features, labels) in enumerate(loader):
+                labels = labels.to(device)
+                features = features.to(device)
+
+                outputs = model(features)
+                loss = criterion(outputs, labels)
+                loss.backward()
+                optimizer.step()
+                optimizer.zero_grad()
+
+                running_loss += loss.item()
+
+                if (i+1) % 50 == 0:
+                    print(f'epoch {epoch+1} / {num_epochs}, step {i+1}/{n_total_steps}, loss = {loss.item():.4f}')
+                    writer.add_scalar('training loss', running_loss / 50, epoch * n_total_steps + i)
+                    running_loss = 0.0
+        writer.close()
--- predictor/nn_binary_classification/utils.py
+++ predictor/nn_binary_classification/utils.py
@@ -4,29 +4,47 @@ import numpy as np
 
 RNA_alphabet = ['A', 'C', 'G', 'U']
 
-def letter_to_index(letter):
-    return RNA_alphabet.index(letter)
+class HueskenDataset(Dataset):
 
-def to_feature(guide_strand, free_energy):
-    feature = np.zeros(85)
-    feature[-1] = free_energy
-    for j, letter in enumerate(guide_strand):
-        letter_array = np.zeros(4)
-        letter_array[letter_to_index(letter)] = 1
-        feature[4*j:4*(j+1)] = letter_array
-    return feature
+    def __init__(self, datafile, transform=None):
+        x, y = self._load_huesken_data(datafile)
+        self.x = x
+        self.y = y
+        self.n_samples = x.shape[0]
+        self.transform = transform
+    
+    def __getitem__(self, index):
+        sample = self.x[index], self.y[index]
+        if self.transform:
+            sample = self.transform(sample)
+        return sample
+    
+    def __len__(self):
+        return self.n_samples
+    
+    def _letter_to_index(self, letter):
+        return RNA_alphabet.index(letter)
 
-def load_huesken_data(datafile, n_samples=None):
-    xy = np.genfromtxt(datafile, delimiter=',', dtype=None, names=True, encoding='utf-8')
-    y = np.zeros((xy.size, 1), dtype=int)
-    y[:,0] = np.rint(xy['norm_inhibitory_activity'])
-    guide_strand = xy['guide_strand']
-    free_energy = xy['free_energy']
-    x = np.zeros((len(free_energy), 85))
-    for i, guide_strand in enumerate(guide_strand):
-        feature = to_feature(guide_strand, free_energy[i])
-        x[i, :] = feature
-    if n_samples:
-        x = x[0:n_samples, :]
-        y = y[0:n_samples]
-    return x, y
+    def _to_feature(self, guide_strand, free_energy):
+        feature = np.zeros(len(guide_strand) + 1)
+        feature[-1] = free_energy
+        for i, letter in enumerate(guide_strand):
+            feature[i] = self._letter_to_index(letter)
+        return feature
+
+    def _load_huesken_data(self, datafile):
+        xy = np.genfromtxt(datafile, delimiter=',', dtype=None, names=True, encoding='utf-8')
+        y = np.zeros((xy.size, 1), dtype=float)
+        y[:,0] = np.rint(xy['norm_inhibitory_activity'])
+        guide_strand = xy['guide_strand']
+        free_energy = xy['free_energy']
+        x = np.zeros((len(free_energy), len(guide_strand[0]) + 1))
+        for i, guide_strand in enumerate(guide_strand):
+            feature = self._to_feature(guide_strand, free_energy[i])
+            x[i, :] = feature
+        return x, y
+
+class ToTensor:
+    def __call__(self, sample):
+        inputs, targets = sample
+        return torch.from_numpy(inputs.astype(np.float32)), torch.from_numpy(targets.astype(np.float32))
