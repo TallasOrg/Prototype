124e137190058fdf956eef25697a9cab347702ab
jahmanson <justin.ahmanson@tallas.io>
2021-05-17 19:03:20 -0500

    rnn linear regression initial results

--- predictor/rnn_linear_regression/training.py
+++ predictor/rnn_linear_regression/training.py
@@ -4,48 +4,47 @@ import torch.nn as nn
 import numpy as np
 import matplotlib.pyplot as plt
 from torch.utils.tensorboard import SummaryWriter
-from utils import HueskenDataset, ToTensor
-from model import nnBinaryClassification
+from utils import HueskenDataset, ToTensor, RNA_alphabet
+from model import rnnLinearRegression
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 dataset = HueskenDataset(datafile='../../data/HueskenRNA_success.csv', transform=ToTensor())
 
-hidden_layers = [1, 2, 3]
-hidden_sizes = [22, 44, 88]
-for num_hidden_layers in hidden_layers:
-    for hidden_size in hidden_sizes:
-        input_size = 22
+for hidden_dim in [10, 20, 30]:
+    for n_layers in [1, 2, 3]:
+        input_size = len(RNA_alphabet) + 1
         num_epochs = 5
         batch_size = 1
         learning_rate = 0.001
         optimizer_type = 'adam'
 
-        LOG_NAME = f'{hidden_size}-nodes-{num_hidden_layers}-dense-{learning_rate}-lr-{optimizer_type}-optim-{int(time.time())}'
+        LOG_NAME = f'{hidden_dim}-hidden_dim-{n_layers}-n_layers-{learning_rate}-lr-{optimizer_type}-optim-{int(time.time())}'
         writer = SummaryWriter(f'logs/{LOG_NAME}')
 
         loader = torch.utils.data.DataLoader(dataset=dataset, batch_size=batch_size, shuffle=True)
         examples = iter(loader)
         example_features, example_labels = examples.next()
 
-        model = nnBinaryClassification(input_size, hidden_size, num_hidden_layers)
-        criterion = nn.BCELoss()
+        model = rnnLinearRegression(input_size, hidden_dim)
+        criterion = nn.MSELoss()
         if optimizer_type == 'adam':
             optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
         else:
             optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
 
-        writer.add_graph(model, example_features)
+        writer.add_graph(model, torch.squeeze(example_features, 0))
 
         n_total_steps = len(loader)
         running_loss = 0.0
         for epoch in range(num_epochs):
             for i, (features, labels) in enumerate(loader):
+                features = torch.squeeze(features, 0)
                 labels = labels.to(device)
                 features = features.to(device)
 
-                outputs = model(features)
-                loss = criterion(outputs, labels)
+                outputs, hidden = model(features)
+                loss = criterion(outputs[-1,:], labels)
                 loss.backward()
                 optimizer.step()
                 optimizer.zero_grad()
@@ -53,7 +52,7 @@ for num_hidden_layers in hidden_layers:
                 running_loss += loss.item()
 
                 if (i+1) % 50 == 0:
-                    print(f'epoch {epoch+1} / {num_epochs}, step {i+1}/{n_total_steps}, loss = {loss.item():.4f}')
+                    print(f'hidden_dim {hidden_dim}, n_layers {n_layers}, epoch {epoch+1} / {num_epochs}, step {i+1}/{n_total_steps}, loss = {loss.item():.4f}')
                     writer.add_scalar('training loss', running_loss / 50, epoch * n_total_steps + i)
                     running_loss = 0.0
         writer.close()
--- predictor/rnn_linear_regression/utils.py
+++ predictor/rnn_linear_regression/utils.py
@@ -7,16 +7,19 @@ RNA_alphabet = ['A', 'C', 'G', 'U']
 class HueskenDataset(Dataset):
 
     def __init__(self, datafile, transform=None):
-        x, y = self._load_huesken_data(datafile)
-        self.x = x
+        guide_strands, free_energy, y = self._load_huesken_data(datafile)
+        self.guide_strands = guide_strands
+        self.free_energy = free_energy
         self.y = y
-        self.n_samples = x.shape[0]
+        self.n_samples = y.shape[0]
         self.transform = transform
     
     def __getitem__(self, index):
-        sample = self.x[index], self.y[index]
+        sample = self._to_feature(self.guide_strands[index], self.free_energy[index]), self.y[index]
+        # sample = self.x[index], self.y[index]
         if self.transform:
             sample = self.transform(sample)
+        inputs, labels = sample
         return sample
     
     def __len__(self):
@@ -26,23 +29,19 @@ class HueskenDataset(Dataset):
         return RNA_alphabet.index(letter)
 
     def _to_feature(self, guide_strand, free_energy):
-        feature = np.zeros(len(line), 1, len(RNA_alphabet) + 1)
+        feature = np.zeros((len(guide_strand), 1, len(RNA_alphabet) + 1))
         for i, letter in enumerate(guide_strand):
-            feature[i][0][self.letter_to_index(letter)] = 1
+            feature[i][0][self._letter_to_index(letter)] = 1
             feature[i][0][-1] = free_energy
         return feature
 
     def _load_huesken_data(self, datafile):
         xy = np.genfromtxt(datafile, delimiter=',', dtype=None, names=True, encoding='utf-8')
         y = np.zeros((xy.size, 1), dtype=float)
-        y[:,0] = np.rint(xy['norm_inhibitory_activity'])
+        y[:,0] = xy['norm_inhibitory_activity']
         guide_strands = xy['guide_strand']
         free_energy = xy['free_energy']
-        x = np.zeros((len(free_energy), len(guide_strands[0]) + 1))
-        for i, guide_strand in enumerate(guide_strands):
-            feature = self._to_feature(guide_strand, free_energy[i])
-            x[i, :] = feature
-        return x, y
+        return guide_strands, free_energy, y
 
 class ToTensor:
     def __call__(self, sample):
