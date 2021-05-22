38a8e10cf147a8250fbd96eb37a34ae8fca37e2f
jahmanson <justin.ahmanson@tallas.io>
2021-05-22 16:55:15 -0500

    rnn model tweak results

---
+++ predictor/rnn_linear_regression_no_free_energy/training.py
@@ -0,0 +1,58 @@
+import time
+import torch
+import torch.nn as nn
+import numpy as np
+import matplotlib.pyplot as plt
+from torch.utils.tensorboard import SummaryWriter
+from utils import HueskenDataset, ToTensor, RNA_alphabet
+from model import rnnLinearRegression
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+dataset = HueskenDataset(datafile='../../data/HueskenRNA_success.csv', transform=ToTensor())
+
+for hidden_dim in [10, 20, 30]:
+    for n_layers in [1, 2, 3]:
+        input_size = len(RNA_alphabet)
+        num_epochs = 5
+        batch_size = 1
+        learning_rate = 0.001
+        optimizer_type = 'adam'
+
+        LOG_NAME = f'{hidden_dim}-hidden_dim-{n_layers}-n_layers-{learning_rate}-lr-{optimizer_type}-optim-{int(time.time())}'
+        writer = SummaryWriter(f'logs/{LOG_NAME}')
+
+        loader = torch.utils.data.DataLoader(dataset=dataset, batch_size=batch_size, shuffle=True)
+        examples = iter(loader)
+        example_features, example_labels = examples.next()
+
+        model = rnnLinearRegression(input_size, hidden_dim, n_layers)
+        criterion = nn.MSELoss()
+        if optimizer_type == 'adam':
+            optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+        else:
+            optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
+
+        writer.add_graph(model, torch.squeeze(example_features, 0))
+
+        n_total_steps = len(loader)
+        running_loss = 0.0
+        for epoch in range(num_epochs):
+            for i, (features, labels) in enumerate(loader):
+                features = torch.squeeze(features, 0)
+                labels = labels.to(device)
+                features = features.to(device)
+
+                outputs, hidden = model(features)
+                loss = criterion(outputs[-1,:], labels)
+                loss.backward()
+                optimizer.step()
+                optimizer.zero_grad()
+
+                running_loss += loss.item()
+
+                if (i+1) % 50 == 0:
+                    print(f'hidden_dim {hidden_dim}, n_layers {n_layers}, epoch {epoch+1} / {num_epochs}, step {i+1}/{n_total_steps}, loss = {loss.item():.4f}')
+                    writer.add_scalar('training loss', running_loss / 50, epoch * n_total_steps + i)
+                    running_loss = 0.0
+        writer.close()
---
+++ predictor/rnn_linear_regression_no_free_energy/utils.py
@@ -0,0 +1,47 @@
+import torch
+from torch.utils.data import Dataset
+import numpy as np
+
+RNA_alphabet = ['A', 'C', 'G', 'U']
+
+class HueskenDataset(Dataset):
+
+    def __init__(self, datafile, transform=None):
+        guide_strands, free_energy, y = self._load_huesken_data(datafile)
+        self.guide_strands = guide_strands
+        self.free_energy = free_energy
+        self.y = y
+        self.n_samples = y.shape[0]
+        self.transform = transform
+    
+    def __getitem__(self, index):
+        sample = self._to_feature(self.guide_strands[index], self.free_energy[index]), self.y[index]
+        if self.transform:
+            sample = self.transform(sample)
+        inputs, labels = sample
+        return sample
+    
+    def __len__(self):
+        return self.n_samples
+    
+    def _letter_to_index(self, letter):
+        return RNA_alphabet.index(letter)
+
+    def _to_feature(self, guide_strand, free_energy):
+        feature = np.zeros((len(guide_strand), 1, len(RNA_alphabet)))
+        for i, letter in enumerate(guide_strand):
+            feature[i][0][self._letter_to_index(letter)] = 1
+        return feature
+
+    def _load_huesken_data(self, datafile):
+        xy = np.genfromtxt(datafile, delimiter=',', dtype=None, names=True, encoding='utf-8')
+        y = np.zeros((xy.size, 1), dtype=float)
+        y[:,0] = xy['norm_inhibitory_activity']
+        guide_strands = xy['guide_strand']
+        free_energy = xy['free_energy']
+        return guide_strands, free_energy, y
+
+class ToTensor:
+    def __call__(self, sample):
+        inputs, targets = sample
+        return torch.from_numpy(inputs.astype(np.float32)), torch.from_numpy(targets.astype(np.float32))
